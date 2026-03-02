4f0d1c674cf28b8a5197d8c2bfffd5445493b78a
jabaln <168536898+jabaln@users.noreply.github.com>
2026-03-02 20:57:30 +0800

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+// login page scripts
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "03-03-2007" && password === "MyNabss") {
+    Swal.fire({
+      icon: "success",
+      title: "Login Success!",
+      text: "Welcome Nabss",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(function () {
+      window.location.href = "birthday.html";
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Login gagal!",
+      text: "Coba cek lagi username sama passwordnya ya:)",
+      confirmButtonText: "Coba lagi",
+      confirmButtonColor: "#ff7675",
+    });
+  }
+}
